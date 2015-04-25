d366a18fad0040f4a353d7de84f826b8c8df9b7d
Kyle Lortscher <kyle91302@gmail.com>
2015-04-25 14:57:26 -0700

    Added like and dislike feature for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
 	def index
-		@recipes = Recipe.all
+		@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
 	end	
 
 	def show
@@ -36,7 +36,19 @@ class RecipesController < ApplicationController
 		else
 			render :edit
 		end		
-	end	
+	end
+
+	def like
+		@recipe = Recipe.find(params[:id])
+		like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+		if like.valid?
+		flash[:success] = "Your selection was successful"
+		redirect_to :back
+		else
+		flash[:danger] = "You can only like/dislike a recipe once."
+		redirect_to :back
+	    end
+	end
 
 	private
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,26 @@
 class Recipe < ActiveRecord::Base
+	belongs_to :chef
+	has_many :likes
 	mount_uploader :picture, PictureUploader
 	belongs_to :chef
 	validates :chef_id, presence: true
 	validates :name, presence: true, length: { in: 5..100 }
 	validates :summary, presence: true, length: { in: 10..150 }
 	validates :description, presence: true, length: { in: 20..500 }
-end	
+
+
+	def thumbs_up_total
+		self.likes.where(like: true).size
+	end
+
+	def thumbs_down_total
+		self.likes.where(like: false).size
+	end
+
+	private
+		def picture_size
+			if picture.size > 5.megabytes
+				errors.add(:picture, "Should be less than 5mb")
+			end
+		end		
+	end	
