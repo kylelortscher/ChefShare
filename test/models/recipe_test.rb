b7a71e2f96aff5a363f6176713f2e471731e2ad2
Kyle Lortscher <kyle91302@gmail.com>
2015-04-24 19:13:32 -0700

    Recipe and Chef Association

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,12 +3,17 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
 	def setup
-		@recipe = Recipe.new(name: "Chicken Parm", summary: "Best Chicken Parm In The World", description: "Heat Oil To 450 degress then add the tomato sauce, onions, then cook chicken for 20 minutes, and finally heat parmersna over it for 15 minutes in oven")
+		@chef = Chef.create(chefname: "bob", email: "bob@example.com")
+		@recipe = @chef.recipes.build(name: "Chicken Parm", summary: "Best Chicken Parm In The World", description: "Heat Oil To 450 degress then add the tomato sauce, onions, then cook chicken for 20 minutes, and finally heat parmersna over it for 15 minutes in oven")
 	end	
 
 	test "Recipe should be valid" do
 		assert @recipe.valid?
 	end
+	test "chef_id should be present" do
+		@recipe.chef_id = nil
+		assert_not @recipe.valid?
+	end	
 	test "name should be presenet" do
 		@recipe.name = " "
 		assert_not @recipe.valid?
